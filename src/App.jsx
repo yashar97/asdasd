c95237908a67a53cf0857d483d44afd0a1c93cb2
Yashar Ghafarinejad <yasharg97@gmail.com>
2024-12-11 07:41:30 -0500

    'a-

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl font-semibold text-center'>Gestion Clientes</h1>
+    </div>
+  )
+}
+
+export default App
